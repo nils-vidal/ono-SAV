274b593afe46c80ca05dcce8d59730596869a509
Matvey Smorodin <mtv.smorodin@gmail.com>
2026-02-18 11:52:38 +0100

    Add ocaml functions for jeu de la vie

--- src/lib/concrete_ono_module.ml
+++ src/lib/concrete_ono_module.ml
@@ -4,20 +4,45 @@ let print_i32 (n : Kdo.Concrete.I32.t) : (unit, _) Result.t =
   Logs.app (fun m -> m "%a" Kdo.Concrete.I32.pp n);
   Ok ()
 
-  
-let print_i64 (n : Kdo.Concrete.I64.t) : (unit, _) Result.t = 
+let print_i64 (n : Kdo.Concrete.I64.t) : (unit, _) Result.t =
   Logs.app (fun m -> m "%a" Kdo.Concrete.I64.pp n);
   Ok ()
 
-let random_i32 () : (Kdo.Concrete.I32.t, _) Result.t = 
+let random_i32 () : (Kdo.Concrete.I32.t, _) Result.t =
   let random_int = Random.int32 Int32.max_int in
   Ok (Kdo.Concrete.I32.of_int32 random_int)
 
+let sleep (seconds : Kdo.Concrete.F32.t) : (unit, _) Result.t =
+  let seconds_float = Kdo.Concrete.F32.to_float seconds in
+  Unix.sleepf seconds_float;
+  Ok ()
+
+let print_cell (cell_id : Kdo.Concrete.I32.t) : (unit, _) Result.t =
+  Logs.app (fun m -> m "%a" Kdo.Concrete.I32.pp cell_id);
+  Ok ()
+
+let newline () : (unit, _) Result.t =
+  Logs.app (fun m -> m "\n");
+  Ok ()
+
+let clear_screen () : (unit, _) Result.t =
+  Logs.app (fun m -> m "\027[2J\027[H");
+  Ok ()
 
 let m =
   let open Kdo.Concrete.Extern_func in
   let open Kdo.Concrete.Extern_func.Syntax in
-  let functions = [ ("print_i32", Extern_func (i32 ^->. unit, print_i32)); ("print_i64", Extern_func (i64 ^->. unit, print_i64)) ; ("random_i32", Extern_func (unit ^->. i32, random_i32))] in
+  let functions =
+    [
+      ("print_i32", Extern_func (i32 ^->. unit, print_i32));
+      ("print_i64", Extern_func (i64 ^->. unit, print_i64));
+      ("random_i32", Extern_func (unit ^->. i32, random_i32));
+      ("sleep", Extern_func (f32 ^->. unit, sleep));
+      ("print_cell", Extern_func (i32 ^->. unit, print_cell));
+      ("newline", Extern_func (unit ^->. unit, newline));
+      ("clear_screen", Extern_func (unit ^->. unit, clear_screen));
+    ]
+  in
   {
     Kdo.Extern.Module.functions;
     func_type = Kdo.Concrete.Extern_func.extern_type;

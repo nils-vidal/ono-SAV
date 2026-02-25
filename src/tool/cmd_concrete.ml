853dfd667d9f700cda3aa1f6c4fbeb66ac01f8ae
AFONSO julien <julien.afonso@etu.u-paris.fr>
2026-02-25 14:40:11 +0100

    hook

--- src/tool/cmd_concrete.ml
+++ src/tool/cmd_concrete.ml
@@ -15,7 +15,10 @@ let info = Cmd.info "concrete" ~exits
 
 let term =
   let open Term.Syntax in
-  let+ () = setup_log and+ source_file = source_file and+ seed = x and+ steps = steps in
+  let+ () = setup_log
+  and+ source_file = source_file
+  and+ seed = x
+  and+ steps = steps in
   Ono.Concrete_driver.run ~source_file ~seed ~steps |> function
   | Ok () -> Ok ()
   | Error e -> Error (`Msg (Kdo.R.err_to_string e))

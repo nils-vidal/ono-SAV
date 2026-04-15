319d00a87466df125271190300aef07a384c4cbb
Matvey Smorodin <mtv.smorodin@gmail.com>
2026-04-15 22:15:42 +0200

    Add sym_cell & fix fill_symbolic bug

--- src/lib/symbolic_ono_module.ml
+++ src/lib/symbolic_ono_module.ml
@@ -15,6 +15,14 @@ let i32_symbol () : Kdo.Symbolic.I32.t Kdo.Symbolic.Choice.t =
   Kdo.Symbolic.Choice.with_new_symbol (Smtml.Ty.Ty_bitv 32)
     Kdo.Symbolic.I32.symbol
 
+let sym_cell () : Kdo.Symbolic.I32.t Kdo.Symbolic.Choice.t =
+  let open Kdo.Symbolic.Choice in
+  let open Kdo.Symbolic.I32 in
+  let* sym = with_new_symbol (Smtml.Ty.Ty_bitv 32) symbol in
+  let* () = assume (le_u sym (of_int 1)) None in
+  let* () = assume (ge_u sym (of_int 0)) None in
+  return sym
+
 let ask_for_a_value () : Kdo.Symbolic.I64.t Kdo.Symbolic.Choice.t =
   print_string "Entrez la valuer de a: ";
   let entry = read_int () in
@@ -51,6 +59,7 @@ let m =
     [
       ("print_i32", Extern_func (i32 ^->. unit, print_i32));
       ("i32_symbol", Extern_func (unit ^->. i32, i32_symbol));
+      ("sym_cell", Extern_func (unit ^->. i32, sym_cell));
       ("mutation_factor", Extern_func (unit ^->. i32, mutation_factor));
       ("ask_for_a_value", Extern_func (unit ^->. i64, ask_for_a_value));
       ("ask_for_b_value", Extern_func (unit ^->. i64, ask_for_b_value));

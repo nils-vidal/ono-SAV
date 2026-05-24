be239af0762d3f521e4a5d6a953ee74f4d834153
Matvey Smorodin <mtv.smorodin@gmail.com>
2026-05-24 15:28:04 +0200

    Use buffered output for TUI

--- src/lib/ui_renderer_terminal.ml
+++ src/lib/ui_renderer_terminal.ml
@@ -1,11 +1,16 @@
-let init () = ()
+let buffer = Buffer.create 1024
+let init () = Buffer.clear buffer
 let on_read_int _ = ()
 let sleep seconds = Unix.sleepf seconds
 
-let print_cell alive =
-  Printf.printf "%s%!" (if alive then "🌸" else "☠️");
-  Unix.sleepf 0.00005
+let print_cell alive = Buffer.add_string buffer (if alive then "🌸" else "☠️")
 
-let newline () = Printf.printf "\n%!"
-let clear_screen () = Printf.printf "\027[H\027[J%!"
+let newline () = Buffer.add_char buffer '\n'
+
+let clear_screen () =
+  print_string "\027[H\027[J\n";
+  Buffer.output_buffer stdout buffer;
+  flush stdout;
+  Buffer.clear buffer
+
 let cleanup () = ()
